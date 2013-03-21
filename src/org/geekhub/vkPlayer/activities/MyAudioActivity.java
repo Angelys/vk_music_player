b3ef3ee63ff0896f5e4057abdce7e9930b250bde
Andrew Sirenko <imalive@ukr.net>
2013-03-21 23:56:13 +0200

    some fixes

--- src/org/geekhub/vkPlayer/activities/MyAudioActivity.java
+++ src/org/geekhub/vkPlayer/activities/MyAudioActivity.java
@@ -1,15 +1,11 @@
 package org.geekhub.vkPlayer.activities;
 
 import android.os.Bundle;
+
+import org.geekhub.vkPlayer.BaseActivity;
 import org.geekhub.vkPlayer.R;
 
-/**
- * Created with IntelliJ IDEA.
- * User: angelys
- * Date: 3/17/13
- * Time: 9:14 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public class MyAudioActivity extends BaseActivity {
 
     public void onCreate(Bundle savedInstanceState){
